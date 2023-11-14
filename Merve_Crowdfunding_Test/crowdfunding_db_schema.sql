a43a85d8be6bae3e9c7595e8692fbbfd493c474b
mervecelme <celme.merve@gmail.com>
2023-11-14 17:34:20 -0500

    Added db verification

--- Merve_Crowdfunding_Test/crowdfunding_db_schema.sql
+++ Merve_Crowdfunding_Test/crowdfunding_db_schema.sql
@@ -4,16 +4,16 @@
 
 
 CREATE TABLE "category" (
-    "category_id" VARCHAR   NOT NULL,
-    "category" VARCHAR   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category" VARCHAR(50)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -21,9 +21,9 @@ CREATE TABLE "subcategory" (
 
 CREATE TABLE "contacts" (
     "contact_id" INTEGER   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "email" VARCHAR   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(100)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
@@ -32,18 +32,18 @@ CREATE TABLE "contacts" (
 CREATE TABLE "campaign" (
     "cf_id" INTEGER   NOT NULL,
     "contact_id" INTEGER   NOT NULL,
-    "company_name" VARCHAR   NOT NULL,
+    "company_name" VARCHAR(100)   NOT NULL,
     "description" VARCHAR   NOT NULL,
     "goal" FLOAT   NOT NULL,
     "pledged" FLOAT   NOT NULL,
-    "outcome" VARCHAR   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
     "backers_count" INTEGER   NOT NULL,
-    "country" VARCHAR   NOT NULL,
-    "currency" VARCHAR   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
     "launched_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR   NOT NULL,
-    "subcategory_id" VARCHAR   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
